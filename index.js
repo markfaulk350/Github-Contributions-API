2dec8dc58ad0baa815346da34712cbf072809ddb
Mark Faulkner <markfaulk350@gmail.com>
2020-02-15 21:58:03 -0800

    basic web scraper works now and will grab a github users commit history for the last year

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cheerio = require('cheerio');
@@ -9,29 +7,61 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({
-//         message: "Please work!"
-//     })
-// })
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Github User Stats Web Scraper Home Route'
+    })
+})
 
-axios.get('https://github.com/markfaulk350').then((res) => {
-    const $ = cheerio.load(res.data);
+app.get('/github-user-stats/:username', (req, res) => {
+    let { username } = req.params;
 
-    // const name = $('.p-name');
-    // console.log(name.html());
-    // console.log(name.text());
-    // or
-    // console.log($('.p-name').html());
-    // console.log($('.p-nickname').text()); // We can use text or html
+    axios.get(`https://github.com/${username}`).then((result) => {
+        const $ = cheerio.load(result.data);
 
-    const calendarGraph = $('.js-calendar-graph-svg').children('g');
+        let days = [];
+        let num_of_commits_this_year = 0;
 
-    console.log(calendarGraph.html())
+        const github_username = $('.p-nickname').text();
+        const full_name = $('.p-name').text();
 
+        const calendarGraph = $('.js-calendar-graph-svg').children('g').children('g');
 
+        calendarGraph.each((i, el) => {
+
+            $(el).find('rect').each((k, elem) => {
+                // let fill = $(elem).attr('fill');
+                let count = parseInt($(elem).attr('data-count'));
+                let date = $(elem).attr('data-date');
+
+                days.push({
+                    count,
+                    date
+                });
+
+                num_of_commits_this_year += count;
+            });
+        });
+
+        // console.log(calendarGraph.html())
+
+        res.status(200).json({
+            github_username,
+            full_name,
+            num_of_commits_this_year,
+            days
+        });
+
+    }).catch((e) => {
+        // console.log(e);
+        res.status(500).json({
+            message: 'Something went wrong with Github user stats Web Scraper!',
+            error: e
+        });
+    });
 });
+
 
 const port = process.env.PORT || 8080;
 
-app.listen(port);
+app.listen(port, () => console.log(`API running on port: ${port}`));
